a2b1fe54164b6cc6d92412f11d828109e888000f
azhagusiva11 <azhagusivas@gmail.com>
2026-02-22 18:51:22 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LuxAdminDemo from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LuxAdminDemo />
-  </React.StrictMode>,
-)
